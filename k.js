711eadaed05e9e25f49d41d4e1211a1cfaa29cc9
Pamela Moreno <60413999+PamelaMoreno@users.noreply.github.com>
2022-05-19 10:46:42 -0500

    Create k.js

---
+++ k.js
@@ -0,0 +1,12 @@
+[
+    {
+	"term": "Kilt",
+	"part": "s.",
+	"definition": "Traje que suelen usar los escoceses en Norteamérica y los norteamericanos en Escocia."
+    },
+    {
+	"term": "Korán",
+	"part": "s.",
+	"definition": "Libro que los mahometanos, neciamente, creen escrito por inspiración divina, pero que los cristianos consideran una perversa impostura, contraria a las Sagradas Escrituras."
+    }
+]
