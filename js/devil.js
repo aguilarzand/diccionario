44138df6908363643574821eeba2a8acaa68b389
LuisOmarSL <luisomarsaldate@gmail.com>
2022-05-19 23:41:55 -0500

    Se agregó la letra G | Luis Omar Saldate

--- js/devil.js
+++ js/devil.js
@@ -34,6 +34,37 @@ $(document).ready(function() {
 	});
 });
 
+
+$(document).ready(function() {
+	$('#letter-g a').click(function(event) {
+		event.preventDefault();
+		$.getJSON('g.json', function(data) {
+			var html = '';
+			$.each(data, function(entryIndex, entry) {
+				html += '<div class="entry">';
+				html += '<h3 class="term">' + entry.term + '</h3>';
+				html += '<div class="part">' + entry.part + '</div>';
+				html += '<div class="definition">';
+				html += entry.definition;
+				if (entry.quote) {
+					html += '<div class="quote">';
+					$.each(entry.quote, function(lineIndex, line) {
+						html += '<div class="quote-line">' + line + '</div>';
+					});
+					if (entry.author) {
+						html += '<div class="quote-author">' + entry.author + '</div>';
+					}
+					html += '</div>';
+				}
+				html += '</div>';
+				html += '</div>';
+			});
+			$('#dictionary').html(html);
+		});
+	});
+});
+
+
 $(document).ready(function() {
 	$('#letter-j j').click(function(event) {
 		event.preventDefault();
